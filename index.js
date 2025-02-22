696392877fd244950ba6ebb92419dd0af69da14f
Abhay Jain <abhayforwork02@gmail.com>
2025-02-22 19:28:40 +0000

    DONE

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+var bodyParser = require('body-parser');
+const isUrl = require("is-url");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+let counter = 1;
+const orgUrl = {};
 
 app.use(cors());
 
@@ -18,6 +25,28 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.post("/api/shorturl", function(req, res) {
+  const url = req.body.url;
+  if (!isUrl(url)) {
+    return res.json({
+      error: "invalid url"
+    })
+  }
+  counter += 1;
+  orgUrl[counter] = url;
+  
+  res.json({
+    original_url: url,
+    short_url: counter
+  })
+})
+
+app.get("/api/shorturl/:id", function(req, res) {
+  const id = req.params.id;
+  const url = orgUrl[id];
+  res.redirect(url);
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
