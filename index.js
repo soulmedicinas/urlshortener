9293ee54ceccf0d067dab3755620c876e3dcfc93
tira_menali <77180304+soulmedicinas@users.noreply.github.com>
2025-04-06 17:45:57 -0700

    add: app.use-express.json

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-
+app.use(express.json());
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
