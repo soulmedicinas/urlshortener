e06423c20eba30779b75a5834e02820097d73dfa
tira_menali <77180304+soulmedicinas@users.noreply.github.com>
2025-04-12 12:18:09 -0700

    add: origin, methods

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST']
+}));
 app.use(express.json());
 app.use('/public', express.static(`${process.cwd()}/public`));
 
