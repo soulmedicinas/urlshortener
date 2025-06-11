43f7bed6108dc4a0a41df96c00485a856d8666b0
tira_menali <77180304+soulmedicinas@users.noreply.github.com>
2025-06-11 16:13:44 -0700

    remove repeat app.listen

--- index.js
+++ index.js
@@ -186,7 +186,3 @@ app.get('/api/hello', function(req, res) {
 app.listen(port, '0.0.0.0', function() {
   console.log(`Server running on port ${port}`);
 });
-
-app.listen(port, '0.0.0.0', function() {
-  console.log(`Server running on port ${port}`);
-});
