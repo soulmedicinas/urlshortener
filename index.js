f551275f9bbd2879eb20fa4b3314142c643a0121
tira_menali <77180304+soulmedicinas@users.noreply.github.com>
2025-06-29 13:54:02 -0700

    add connect MongoDB

--- index.js
+++ index.js
@@ -6,15 +6,12 @@ const mongoose = require('mongoose');
 const dns = require('dns');
 const app = express();
 
-
-
-
-// Setup __dirname equivalent in ES Modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const app = express();
+// Basic Configuration
 const port = process.env.PORT || 3000;
+
+// Database
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 app.use(cors());
 app.use(express.json());
